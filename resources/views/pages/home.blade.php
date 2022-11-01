6c2cba37adf80135fdbbeb117319fe085ff02a49
= <=>
2022-11-01 10:44:30 +0700

    sdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -101,29 +101,29 @@
     
 
     <div class="section dangky">
-        <h2>ĐĂNG KÝ THÔNG TIN</h2>
+        <h2>REGISTER INFORMATION</h2>
         <form action="" method="post">
-            <input type="text" name="name" class="form-control" placeholder="Họ và Tên">
-            <input type="text" name="phone" class="form-control" placeholder="Số điện thoại">
-            <input type="email" name="email" class="form-control" placeholder="Địa chỉ email">
+            <input type="text" name="name" class="form-control" placeholder="Your Name">
+            <input type="text" name="phone" class="form-control" placeholder="Phone number">
+            <input type="email" name="email" class="form-control" placeholder="Email">
             <p><div class="g-recaptcha" data-sitekey="6LfsErkiAAAAABhpVqPBI85ByiHOUvdQ-h2TT_X2"></div></p>
-            <button class="form-control" type="submit">Đăng Ký</button>
+            <button class="form-control" type="submit">REGISTER</button>
         </form>
     </div>
     <div class="section lienhe">
-        <h2>LIÊN HỆ</h2>
+        <h2>CONTACT</h2>
         <div><img src="imgs/logo.png"></div>
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <h5>LIÊN HỆ</h5>
-                    <p>Số điện thoại: 1900 23 23 89</p>
+                    <h5>CONTACT</h5>
+                    <p>Phone: 1900 23 23 89</p>
                     <p>Email: info@vinhomes.vn</p>
-                    <p>Địa chỉ giao dịch: Trung tâm giao dịch Bất động sản Vinhomes, L3-01, Tầng 2, TTTM Vincom Mega Mall, Vinhomes Ocean Park, Gia Lâm, Hà Nội</p>
+                    <p>Address: Trung tâm giao dịch Bất động sản Vinhomes, L3-01, Tầng 2, TTTM Vincom Mega Mall, Vinhomes Ocean Park, Gia Lâm, Hà Nội</p>
                 </div>
                 <div class="col-lg-6">
-                    <h5>CHÚ Ý</h5>
-                    <p>*Thông tin, hình ảnh, các tiện ích trên website chỉ mang tính chất tương đối và có thể được điều chỉnh theo quyết định của Chủ đầu tư tại từng thời điểm đảm bảo phù hợp quy hoạch và thực tế thi công Dự án. Các thông tin, cam kết chính thức sẽ được quy định cụ thể tại Hợp đồng mua bán. Việc quản lý, vận hành và kinh doanh của khu đô thị sẽ theo quy định của Ban quản lý.</p>
+                    <h5>ATTENTION</h5>
+                    <p>*Information, images and utilities on the website are approximate and can be adjusted according to the decision of the Investor from time to time to ensure compliance with the planning and actual construction of the Project. The official information and commitments will be specified in the Sales Contract. The management, operation and business of the urban area will follow the regulations of the Management Board.</p>
                 </div>
             </div>
         </div>
